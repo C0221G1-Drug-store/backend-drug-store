3921660a177b6b8a30b4df413ab25d23fbc722f5
Your Name <you@example.com>
2021-08-16 20:10:32 +0700

    Update 16/08

--- pharmacy_management/src/main/java/com/backend/pharmacy_management/model/entity/indicative_prescription/Prescription.java
+++ pharmacy_management/src/main/java/com/backend/pharmacy_management/model/entity/indicative_prescription/Prescription.java
@@ -16,78 +16,16 @@ import java.util.List;
 public class Prescription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long prescriptionId;  // id đơn thuốc
-    private String prescriptionCode;  // mã code
-    private String prescriptionName;   // tên đơn thuốc
-    private String symptom;    // triệu chứng
-    private String object;     // đối tượng uống thuốc
-    private int numberOfDay;  // số ngày uống
-    private  String note;  //  ghi chú
-    @JsonBackReference
-    @OneToMany(targetEntity = PrescriptionIndicative.class)
-    private List<PrescriptionIndicative> prescriptionIndicatives;
+    private Long prescriptionId;
+    private String prescriptionCode;
+    private String prescriptionName;
+    private String symptom;
+    private String object;
+    private int numberOfDay;
+    private  String note;
+    private  boolean flag;
+    @ManyToOne
+    @JoinColumn(name = "indicative_id", referencedColumnName = "indicativeId")
+    private  Indicative indicative;
 
-    public Long getPrescriptionId() {
-        return prescriptionId;
-    }
-
-    public void setPrescriptionId(Long prescriptionId) {
-        this.prescriptionId = prescriptionId;
-    }
-
-    public String getPrescriptionCode() {
-        return prescriptionCode;
-    }
-
-    public void setPrescriptionCode(String prescriptionCode) {
-        this.prescriptionCode = prescriptionCode;
-    }
-
-    public String getPrescriptionName() {
-        return prescriptionName;
-    }
-
-    public void setPrescriptionName(String prescriptionName) {
-        this.prescriptionName = prescriptionName; 
-    }
-
-    public String getSymptom() {
-        return symptom;
-    }
-
-    public void setSymptom(String symptom) {
-        this.symptom = symptom;
-    }
-
-    public String getObject() {
-        return object;
-    }
-
-    public void setObject(String object) {
-        this.object = object;
-    }
-
-    public int getNumberOfDay() {
-        return numberOfDay;
-    }
-
-    public void setNumberOfDay(int numberOfDay) {
-        this.numberOfDay = numberOfDay;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public List<PrescriptionIndicative> getPrescriptionIndicatives() {
-        return prescriptionIndicatives;
-    }
-
-    public void setPrescriptionIndicatives(List<PrescriptionIndicative> prescriptionIndicatives) {
-        this.prescriptionIndicatives = prescriptionIndicatives;
-    }
 }
