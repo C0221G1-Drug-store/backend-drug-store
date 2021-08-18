cff4c8e605f5788a94c2edda4f86db2365ea26b1
acuonghero <acuonghero@gmail.com>
2021-08-18 16:57:05 +0700

    Cuong update ngay 18/8

--- pharmacy_management/src/main/java/com/backend/pharmacy_management/controller/employee/EmployeeController.java
+++ pharmacy_management/src/main/java/com/backend/pharmacy_management/controller/employee/EmployeeController.java
@@ -16,24 +16,4 @@ public class EmployeeController {
     @PostMapping("/create")
     public void save(@RequestBody Employee employee) {
          employeeService.save(employee);
-    }
-
-//    @PostMapping("/create")
-//    public ResponseEntity<Employee> addNewEmployee(@RequestBody Employee employee) {
-//
-//        // Check provider
-//
-//        // Create vaccine
-//        vaccineService.createVaccine(createVaccineDTO);
-//
-//        Vaccine vaccine = vaccineService.searchName(createVaccineDTO.getNameVaccine());
-//
-//        storageService.createStorage((int) createVaccineDTO.getQuantity(), vaccine.getVaccineId());
-//        invoiceService.createInvoice(createVaccineDTO.getExpired(), createVaccineDTO.getUnitPrice(),
-//                (int) createVaccineDTO.getQuantity(), createVaccineDTO.getDayReceive(),
-//                provider.getProviderId(), vaccine.getVaccineId());
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//
-//        return new ResponseEntity<Void>(httpHeaders, HttpStatus.OK);
-//    }
-}
+    }}
