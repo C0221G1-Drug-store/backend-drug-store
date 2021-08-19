e7a995abb69559c536624303b8a47fe3d98462d6
Trang Nguyễn <trangnguyenfcp@gmail.com>
2021-08-19 09:27:09 +0700

    sua loi git

--- pharmacy_management/src/main/java/com/backend/pharmacy_management/model/entity/drug/Drug.java
+++ pharmacy_management/src/main/java/com/backend/pharmacy_management/model/entity/drug/Drug.java
@@ -6,16 +6,16 @@ import com.backend.pharmacy_management.model.entity.import_bill_payment.ImportBi
 import com.backend.pharmacy_management.model.entity.indicative_prescription.Indicative;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.List;
 
 @Entity
 @Data
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Table(name="drug")
 public class Drug {
@@ -29,11 +29,7 @@ public class Drug {
     private String drugName;
     @Column(name = "active_element")
     private String activeElement;
-<<<<<<< HEAD
     @Column(name = "drug_amount")
-=======
-    @Column(name = "drug_amout")
->>>>>>> 683bfc936f0fbf0a0042a0eb4d00f0057fb37ca6
     private Long drugAmount;
     @Column(name = "unit")
     private String unit;
@@ -53,208 +49,24 @@ public class Drug {
     private String manufacturer;
     private String origin;
     private String note;
-<<<<<<< HEAD
     @ManyToOne
     @JoinColumn(name = "drug_group_id")
-    @JsonBackReference(value = "drug_group-drug")
+    @JsonBackReference(value = "drug_group_drug")
     private DrugGroup drugGroup;
-=======
-    @Column(columnDefinition = "TEXT")
-    private String drugSideEffect;
-    private Boolean flag = true;
-//    @ManyToOne
-//    @JoinColumn(name = "drug_group_id")
-    @Column(name = "drug_group_id")
-    private String drugGroup;
->>>>>>> 683bfc936f0fbf0a0042a0eb4d00f0057fb37ca6
     @OneToMany(mappedBy = "drug")
-    @JsonManagedReference(value = "drug_image_detail-drug")
+    @JsonManagedReference(value = "drug_image_detail_drug")
     private List<DrugImageDetail> drugImageDetails;
     @OneToMany(mappedBy = "drug")
-    @JsonManagedReference(value = "drug_of_bill-drug")
+    @JsonManagedReference(value = "drug_of_bill_drug")
     private List<DrugOfBill> drugOfBills;
     @OneToMany(mappedBy = "drug")
-    @JsonManagedReference(value = "indicatives_drug-drug")
+    @JsonManagedReference(value = "indicatives_drug_drug")
     private List<Indicative> indicatives;
     @OneToMany(mappedBy = "drug")
-    @JsonManagedReference(value = "export_bill_detail-drug")
+    @JsonManagedReference(value = "export_bill_detail_drug")
     private List<ExportBillDetail> exportBillDetails;
-<<<<<<< HEAD
     @OneToMany(mappedBy = "drug")
-    @JsonManagedReference(value = "import_bill_drug-drug")
+    @JsonManagedReference(value = "import_bill_drug_drug")
     private List<ImportBillDrug> importBillDrugs;
 
-=======
-
-    public Drug() {
-    }
-
-    public Long getDrugId() {
-        return drugId;
-    }
-
-    public void setDrugId(Long drugId) {
-        this.drugId = drugId;
-    }
-
-    public String getDrugCode() {
-        return drugCode;
-    }
-
-    public void setDrugCode(String drugCode) {
-        this.drugCode = drugCode;
-    }
-
-    public String getDrugName() {
-        return drugName;
-    }
-
-    public void setDrugName(String drugName) {
-        this.drugName = drugName;
-    }
-
-    public String getActiveElement() {
-        return activeElement;
-    }
-
-    public void setActiveElement(String activeElement) {
-        this.activeElement = activeElement;
-    }
-
-    public Long getDrugAmount() {
-        return drugAmount;
-    }
-
-    public void setDrugAmount(Long drugAmount) {
-        this.drugAmount = drugAmount;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public String getConversionUnit() {
-        return conversionUnit;
-    }
-
-    public void setConversionUnit(String conversionUnit) {
-        this.conversionUnit = conversionUnit;
-    }
-
-    public Integer getConversionRate() {
-        return conversionRate;
-    }
-
-    public void setConversionRate(Integer conversionRate) {
-        this.conversionRate = conversionRate;
-    }
-
-    public Double getWholesaleProfitRate() {
-        return wholesaleProfitRate;
-    }
-
-    public void setWholesaleProfitRate(Double wholesaleProfitRate) {
-        this.wholesaleProfitRate = wholesaleProfitRate;
-    }
-
-    public Double getRetailProfitRate() {
-        return retailProfitRate;
-    }
-
-    public void setRetailProfitRate(Double retailProfitRate) {
-        this.retailProfitRate = retailProfitRate;
-    }
-
-    public String getDrugFaculty() {
-        return drugFaculty;
-    }
-
-    public void setDrugFaculty(String drugFaculty) {
-        this.drugFaculty = drugFaculty;
-    }
-
-    public String getManufacturer() {
-        return manufacturer;
-    }
-
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public String getDrugSideEffect() {
-        return drugSideEffect;
-    }
-
-    public void setDrugSideEffect(String drugSideEffect) {
-        this.drugSideEffect = drugSideEffect;
-    }
-
-    public Boolean getFlag() {
-        return flag;
-    }
-
-    public void setFlag(Boolean flag) {
-        this.flag = flag;
-    }
-
-    public String getDrugGroup() {
-        return drugGroup;
-    }
-
-    public void setDrugGroup(String drugGroup) {
-        this.drugGroup = drugGroup;
-    }
-
-    public List<DrugImageDetail> getDrugImageDetails() {
-        return drugImageDetails;
-    }
-
-    public void setDrugImageDetails(List<DrugImageDetail> drugImageDetails) {
-        this.drugImageDetails = drugImageDetails;
-    }
-
-    public List<DrugOfBill> getDrugOfBills() {
-        return drugOfBills;
-    }
-
-    public void setDrugOfBills(List<DrugOfBill> drugOfBills) {
-        this.drugOfBills = drugOfBills;
-    }
-
-    public List<Indicative> getIndicatives() {
-        return indicatives;
-    }
-
-    public void setIndicatives(List<Indicative> indicatives) {
-        this.indicatives = indicatives;
-    }
-
-    public List<ExportBillDetail> getExportBillDetails() {
-        return exportBillDetails;
-    }
-
-    public void setExportBillDetails(List<ExportBillDetail> exportBillDetails) {
-        this.exportBillDetails = exportBillDetails;
-    }
->>>>>>> 683bfc936f0fbf0a0042a0eb4d00f0057fb37ca6
 }
