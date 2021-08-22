22756ee9cc9d6391c6ac706f04a6f94bfbc36089
minh230998 <minhnguyenduc27121998@gmail.com>
2021-08-22 15:04:23 +0700

    up bai

--- pharmacy_management/src/main/java/com/backend/pharmacy_management/model/repository/manufacturer/ManufacturerRepository.java
+++ pharmacy_management/src/main/java/com/backend/pharmacy_management/model/repository/manufacturer/ManufacturerRepository.java
@@ -13,25 +13,25 @@ import java.util.List;
 public interface ManufacturerRepository extends PagingAndSortingRepository<Manufacturer, Long> {
     @Query(value = "select * from manufacturer where flag = 1", nativeQuery = true)
     Page<Manufacturer> findAllManufacturer(Pageable pageable);
-    @Query(value = "select * from manufacturer where manufacturer_code like %?1%", nativeQuery = true)
+    @Query(value = "select * from manufacturer where manufacturer_code like %?1% and flag = 1", nativeQuery = true)
     Page<Manufacturer> findByManufacturerCode(String code, Pageable pageable);
-    @Query(value = "select * from manufacturer where manufacturer_name like %?1%", nativeQuery = true)
+    @Query(value = "select * from manufacturer where manufacturer_name like %?1% and flag = 1", nativeQuery = true)
     Page<Manufacturer> findByManufacturerName(String name, Pageable pageable);
-    @Query(value = "select * from manufacturer where manufacturer_address like %?1%", nativeQuery = true)
+    @Query(value = "select * from manufacturer where manufacturer_address like %?1% and flag = 1", nativeQuery = true)
     Page<Manufacturer> findByManufacturerAddress(String address, Pageable pageable);
-    @Query(value = "select * from manufacturer where manufacturer_note like %?1%", nativeQuery = true)
+    @Query(value = "select * from manufacturer where manufacturer_note like %?1% and flag = 1", nativeQuery = true)
     Page<Manufacturer> findByManufacturerNote(String node, Pageable pageable);
-    @Query(value = "select * from manufacturer where manufacturer_phone_number like %?1%", nativeQuery = true)
+    @Query(value = "select * from manufacturer where manufacturer_phone_number like %?1% and flag = 1", nativeQuery = true)
     Page<Manufacturer> findByManufacturerPhoneNumber(String phoneNumber, Pageable pageable);
-    @Query(value = "select * from manufacturer order by manufacturer_name", nativeQuery = true)
+    @Query(value = "select * from manufacturer where flag = 1 order by manufacturer_name", nativeQuery = true)
     Page<Manufacturer> findAllSortName(Pageable pageable);
-    @Query(value = "select * from manufacturer order by manufacturer_code", nativeQuery = true)
+    @Query(value = "select * from manufacturer where flag = 1 order by manufacturer_code", nativeQuery = true)
     Page<Manufacturer> findAllSortCode(Pageable pageable);
-    @Query(value = "select * from manufacturer order by manufacturer_address", nativeQuery = true)
+    @Query(value = "select * from manufacturer where flag = 1 order by manufacturer_address", nativeQuery = true)
     Page<Manufacturer> findAllSortAddress(Pageable pageable);
-    @Query(value = "select * from manufacturer order by manufacturer_note", nativeQuery = true)
+    @Query(value = "select * from manufacturer where flag = 1 order by manufacturer_note", nativeQuery = true)
     Page<Manufacturer> findAllSortNote(Pageable pageable);
-    @Query(value = "select * from manufacturer order by manufacturer_phone_number", nativeQuery = true)
+    @Query(value = "select * from manufacturer where flag = 1 order by manufacturer_phone_number", nativeQuery = true)
     Page<Manufacturer> findAllSortPhoneNumber(Pageable pageable);
     @Query(value= "update manufacturer set flag = 0 WHERE manufacturer_id = ?1 ", nativeQuery = true)
     void deleteByIdManufacturer(Long id);
