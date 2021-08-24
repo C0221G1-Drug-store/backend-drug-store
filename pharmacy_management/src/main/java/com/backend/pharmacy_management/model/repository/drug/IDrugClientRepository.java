47866bc0a6fc5f3fd18530a68f092f49af378f7e
Huynh Nguyen <huynhnguyen@Huynhs-MacBook-Pro.local>
2021-08-24 19:23:14 +0700

    Huynh updated Client Queries

--- pharmacy_management/src/main/java/com/backend/pharmacy_management/model/repository/drug/IDrugClientRepository.java
+++ pharmacy_management/src/main/java/com/backend/pharmacy_management/model/repository/drug/IDrugClientRepository.java
@@ -29,7 +29,7 @@ public interface IDrugClientRepository extends PagingAndSortingRepository<Drug, 
             " (sum(ibd.discount_rate*ibd.import_amount))/(sum(ibd.import_amount)) as discountRate, (sum(ibd.vat*ibd.import_amount))/(sum(ibd.import_amount)) as vat, \n" +
             " (sum(ibd.import_price*ibd.import_amount))/(sum(ibd.import_amount)) as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount*ibd.import_amount))/(sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
             "d.wholesale_profit_rate as wholesaleProfitRate, d.retail_profit_rate as retailProfitRate, d.drug_faculty as drugFaculty, d.manufacturer as manufacturer, d.origin as origin,\n" +
-            "d.note as note, d.drug_side_effect as drugSideEffect, dg.drug_group_name as drugGroup ,di.drug_image_detail_url as drugImageDetails \n" +
+            "d.note as note, d.drug_side_effect as drugSideEffect, dg.drug_group_name as drugGroup, di.drug_image_detail_url as drugImageDetails \n" +
             "from drug d \n" +
             "left join drug_group dg on d.drug_group_id = dg.drug_group_id\n" +
             "left join import_bill_drug ibd on d.drug_id = ibd.drug_id\n" +
@@ -39,10 +39,11 @@ public interface IDrugClientRepository extends PagingAndSortingRepository<Drug, 
     List<ListDrugDTO> findAllDrugsNotPagination();
 
     @Query(value = "select ((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.wholesale_profit_rate/100))*ibd.import_amount))/(sum(ibd.import_amount))) as wholesalePrice,\n" +
-            "((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.retail_profit_rate/100))*ibd.import_amount)/(sum(ibd.import_amount))/d.conversion_rate) as retailPrice,\n" +
-            " ibd.discount_rate as discountRate, ibd.vat as vat, ibd.import_price as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
+            "((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.retail_profit_rate/100))*ibd.import_amount)/(sum(ibd.import_amount)))/d.conversion_rate) as retailPrice,\n" +
+            " (sum(ibd.discount_rate*ibd.import_amount))/(sum(ibd.import_amount)) as discountRate, (sum(ibd.vat*ibd.import_amount))/(sum(ibd.import_amount)) as vat, \n" +
+            " (sum(ibd.import_price*ibd.import_amount))/(sum(ibd.import_amount)) as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount*ibd.import_amount))/(sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
             "d.wholesale_profit_rate as wholesaleProfitRate, d.retail_profit_rate as retailProfitRate, d.drug_faculty as drugFaculty, d.manufacturer as manufacturer, d.origin as origin,\n" +
-            "d.note as note, d.flag as flag, dg.drug_group_name as drugGroup, d.drug_side_effect as drugSideEffect, di.drug_image_detail_url as drugImageDetails \n" +
+            "d.note as note, d.drug_side_effect as drugSideEffect, dg.drug_group_name as drugGroup, di.drug_image_detail_url as drugImageDetails \n" +
             "from drug d \n" +
             "left join drug_group dg on d.drug_group_id = dg.drug_group_id\n" +
             "left join import_bill_drug ibd on d.drug_id = ibd.drug_id\n" +
@@ -52,10 +53,11 @@ public interface IDrugClientRepository extends PagingAndSortingRepository<Drug, 
     List<ListDrugDTO> searchDrugs(String searchTerm);
 
     @Query(value = "select ((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.wholesale_profit_rate/100))*ibd.import_amount))/(sum(ibd.import_amount))) as wholesalePrice,\n" +
-            "((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.retail_profit_rate/100))*ibd.import_amount)/(sum(ibd.import_amount))/d.conversion_rate) as retailPrice,\n" +
-            " ibd.discount_rate as discountRate, ibd.vat as vat, ibd.import_price as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
+            "((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.retail_profit_rate/100))*ibd.import_amount)/(sum(ibd.import_amount)))/d.conversion_rate) as retailPrice,\n" +
+            " (sum(ibd.discount_rate*ibd.import_amount))/(sum(ibd.import_amount)) as discountRate, (sum(ibd.vat*ibd.import_amount))/(sum(ibd.import_amount)) as vat, \n" +
+            " (sum(ibd.import_price*ibd.import_amount))/(sum(ibd.import_amount)) as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount*ibd.import_amount))/(sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
             "d.wholesale_profit_rate as wholesaleProfitRate, d.retail_profit_rate as retailProfitRate, d.drug_faculty as drugFaculty, d.manufacturer as manufacturer, d.origin as origin,\n" +
-            "d.note as note, d.flag as flag, dg.drug_group_name as drugGroup, d.drug_side_effect as drugSideEffect, di.drug_image_detail_url as drugImageDetails \n" +
+            "d.note as note, d.drug_side_effect as drugSideEffect, dg.drug_group_name as drugGroup, di.drug_image_detail_url as drugImageDetails \n" +
             "from drug d \n" +
             "left join drug_group dg on d.drug_group_id = dg.drug_group_id\n" +
             "left join import_bill_drug ibd on d.drug_id = ibd.drug_id\n" +
@@ -72,10 +74,11 @@ public interface IDrugClientRepository extends PagingAndSortingRepository<Drug, 
     DrugDTO findDrugById(Long id);
 
     @Query(value = "select ((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.wholesale_profit_rate/100))*ibd.import_amount))/(sum(ibd.import_amount))) as wholesalePrice,\n" +
-            "((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.retail_profit_rate/100))*ibd.import_amount)/(sum(ibd.import_amount))/d.conversion_rate) as retailPrice,\n" +
-            " ibd.discount_rate as discountRate, ibd.vat as vat, ibd.import_price as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
+            "((sum((ibd.import_price*(1-ibd.discount_rate/100)*(1+ibd.vat/100)*(1+d.retail_profit_rate/100))*ibd.import_amount)/(sum(ibd.import_amount)))/d.conversion_rate) as retailPrice,\n" +
+            " (sum(ibd.discount_rate*ibd.import_amount))/(sum(ibd.import_amount)) as discountRate, (sum(ibd.vat*ibd.import_amount))/(sum(ibd.import_amount)) as vat, \n" +
+            " (sum(ibd.import_price*ibd.import_amount))/(sum(ibd.import_amount)) as importPrice, d.drug_id as drugId, d.drug_code as drugCode, d.drug_name as drugName, d.active_element as activeElement, (sum(ibd.import_amount*ibd.import_amount))/(sum(ibd.import_amount)) as drugAmount, d.unit as unit, d.conversion_unit as conversionUnit, d.conversion_rate as conversionRate,\n" +
             "d.wholesale_profit_rate as wholesaleProfitRate, d.retail_profit_rate as retailProfitRate, d.drug_faculty as drugFaculty, d.manufacturer as manufacturer, d.origin as origin,\n" +
-            "d.note as note, d.flag as flag, dg.drug_group_name as drugGroup, d.drug_side_effect as drugSideEffect, di.drug_image_detail_url as drugImageDetails \n" +
+            "d.note as note, d.drug_side_effect as drugSideEffect, dg.drug_group_name as drugGroup, di.drug_image_detail_url as drugImageDetails \n" +
             "from drug d \n" +
             "left join drug_group dg on d.drug_group_id = dg.drug_group_id\n" +
             "left join import_bill_drug ibd on d.drug_id = ibd.drug_id\n" +
